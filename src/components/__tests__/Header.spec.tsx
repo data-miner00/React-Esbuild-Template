9b91e1c850e016373ddd8eec5a61ee4757644300
Shaun Chong <mumk0313@gmail.com>
2022-11-05 10:38:00 +0800

    test: Fix no router for link component

--- src/components/__tests__/Header.spec.tsx
+++ src/components/__tests__/Header.spec.tsx
@@ -2,9 +2,14 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 
 import { Header } from "../Header";
+import { MemoryRouter } from "react-router-dom";
 
 it("renders the content properly", () => {
-  render(<Header />);
+  render(
+    <MemoryRouter>
+      <Header />
+    </MemoryRouter>
+  );
   const helloElement = screen.getByText(/logod/i);
   expect(helloElement).toBeInTheDocument();
 });
