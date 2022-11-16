fa93e268fa00c34d7f6341cd469c4b8e809e6a80
Shaun Chong <mumk0313@gmail.com>
2022-11-17 00:03:06 +0800

    fix(style): Update dark mode styles

--- src/components/StackCard.tsx
+++ src/components/StackCard.tsx
@@ -11,11 +11,11 @@ function StackCard({ title, description, url }: Props): JSX.Element {
     <a
       href={url}
       target="_blank"
-      className="block p-6 w-80 group group-hover:shadow-md border border-solid border-gray-500 rounded hover:border-gray-400"
+      className="block p-6 w-80 group group-hover:shadow-md border border-solid border-gray-300 hover:border-gray-400 dark:border-gray-500 rounded dark:hover:border-gray-400"
     >
       <h4 className="font-bold text-lg">{title}</h4>
-      <p className="my-3 text-gray-300">{description}</p>
-      <button className="text-blue-400 group-hover:underline hover:text-blue-300">
+      <p className="my-3 text-gray-500 dark:text-gray-300">{description}</p>
+      <button className="text-blue-400 dark:text-purple-400 group-hover:underline hover:text-blue-300">
         Documentation →
       </button>
     </a>
