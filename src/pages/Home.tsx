0517dac50cae4114dd4f4fff24fda634ac3080d6
Shaun Chong <mumk0313@gmail.com>
2022-11-18 23:39:22 +0800

    feat: Use simple motion

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { motion } from "framer-motion";
 
 import StackCard from "../components/StackCard";
 import techData from "../data/TechnologyData.json";
@@ -7,16 +8,49 @@ function Home(): JSX.Element {
   return (
     <main className="">
       <header className="mt-4 mb-16">
-        <h1 className="my-awesome-h1">The Ultimate Template</h1>
-        <p className="text-center text-gray-500 dark:text-gray-300">
+        <motion.h1
+          className="my-awesome-h1"
+          initial={{
+            opacity: 0,
+            y: "100%",
+          }}
+          animate={{
+            animationDuration: "500ms",
+            opacity: 1,
+            y: 0,
+          }}
+          transition={{
+            type: "tween",
+          }}
+        >
+          The Ultimate Template
+        </motion.h1>
+        <motion.p
+          initial={{
+            opacity: 0,
+            y: "100%",
+          }}
+          animate={{
+            animationDuration: "500ms",
+            opacity: 1,
+            y: 0,
+          }}
+          transition={{
+            ease: "easeOut",
+            delay: 0.3,
+            type: "tween",
+          }}
+          className="text-center text-gray-500 dark:text-gray-300"
+        >
           Batteries included React template that supercharges your development.
-        </p>
+        </motion.p>
       </header>
 
       <div className="mx-auto w-2/3 flex flex-wrap gap-7 justify-center items-center">
         {techData.map((data, index) => (
           <StackCard
             key={index}
+            delay={index}
             title={data.title}
             description={data.description}
             url={data.url}
