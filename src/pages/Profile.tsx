f82f70a5ade44f3786d1d5cfd7e412c918abb5f8
data-miner00 <data-miner00@users.noreply.github.com>
2022-11-24 22:38:59 +0800

    fix: Update showcase for env

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,9 +1,13 @@
 import React from "react";
+import { getVariable, Variable } from "../utils/getVariable";
 
 function Profile(): JSX.Element {
   return (
     <>
-      <h1>Profile page {process.env.REACT_APP_SECRET}</h1>
+      <h1>
+        Profile page {getVariable(Variable.REACT_APP_SECRET)} -{" "}
+        {getVariable<number>(Variable.REACT_APP_NUMBER)}
+      </h1>
     </>
   );
 }
