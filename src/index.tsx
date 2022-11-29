8083d4dc6cc6d551c3d989432e6bcbdf8aa21145
data-miner00 <data-miner00@users.noreply.github.com>
2022-11-29 21:49:54 +0800

    feat: Added basic i18n

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,8 @@ import { BrowserRouter } from "react-router-dom";
 
 import App from "./App";
 
+import "./i18n";
+
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
---
+++ src/locales/index.ts
@@ -0,0 +1,11 @@
+import en from "./en.json";
+import ja from "./ja.json";
+
+export default {
+  en: {
+    translation: en,
+  },
+  ja: {
+    translation: ja,
+  },
+};
