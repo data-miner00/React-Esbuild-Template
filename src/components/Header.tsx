ffee83c09cec550990dfd3e6ee064756caf1b581
data-miner00 <data-miner00@users.noreply.github.com>
2022-11-30 18:50:57 +0800

    feat: Added language switcher component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 import ThemeSwitcher from "./ThemeSwitcher";
+import LanguageSwitcher from "./LanguageSwitcher";
 
 function Header(): JSX.Element {
   return (
@@ -21,6 +22,7 @@ function Header(): JSX.Element {
             About
           </Link>
           <ThemeSwitcher />
+          <LanguageSwitcher />
         </nav>
       </div>
     </header>
