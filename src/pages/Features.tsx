67a0b6d2a2e4bc99af432c324b68b314b6abca17
data-miner00 <data-miner00@users.noreply.github.com>
2022-12-04 16:31:51 +0800

    feat: Updated features page

--- src/pages/Features.tsx
+++ src/pages/Features.tsx
@@ -2,13 +2,36 @@ import React from "react";
 import { getVariable, Variable } from "../utils/getVariable";
 
 function Features(): JSX.Element {
+  const features: Array<string> = [
+    "Latest React 18",
+    "Latest Tailwind v3",
+    "Jest with @testing-library/react configured",
+    "Prettier formatter",
+    "Dark mode",
+    "Internationalization",
+    "Configurable linter rules",
+    "Routes configured",
+    "Not Found page",
+    "Loader",
+    "Environment variables & helpers",
+    "Pnpm package manager",
+    "GitHub workflows",
+  ];
+
   return (
-    <>
-      <h1>
-        Profile page {getVariable(Variable.REACT_APP_SECRET)} -{" "}
-        {getVariable<number>(Variable.REACT_APP_NUMBER)}
+    <div className="max-w-[1400px] mx-auto md:px-8">
+      <h1 className="text-xl font-bold mb-10">
+        List of features that are baked into this template
       </h1>
-    </>
+
+      <ul className="flex flex-col gap-2">
+        {features.map((feature) => (
+          <li className="border dark:border-gray-600 border-gray-200 border-solid p-3">
+            ✨ {feature}
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 }
 
