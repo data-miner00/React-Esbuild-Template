96788c5af8e0e4bbc65f6b29259fa5fa36b904b6
data-miner00 <data-miner00@users.noreply.github.com>
2022-12-05 22:33:40 +0800

    refactor: Move features to data folder

--- src/pages/Features.tsx
+++ src/pages/Features.tsx
@@ -1,31 +1,16 @@
 import React from "react";
 import { getVariable, Variable } from "../utils/getVariable";
+import featuresData from "../data/Features.json";
 
 function Features(): JSX.Element {
-  const features: Array<string> = [
-    "Latest React 18",
-    "Latest Tailwind v3",
-    "Jest with @testing-library/react configured",
-    "Prettier formatter",
-    "Dark mode",
-    "Internationalization",
-    "Configurable linter rules",
-    "Routes configured",
-    "Not Found page",
-    "Loader",
-    "Environment variables & helpers",
-    "Pnpm package manager",
-    "GitHub workflows",
-  ];
-
   return (
     <div className="max-w-[1400px] mx-auto md:px-8">
       <h1 className="text-xl font-bold mb-10">
-        List of features that are baked into this template
+        Detailed list of features that are baked into this template
       </h1>
 
       <ul className="flex flex-col gap-2">
-        {features.map((feature) => (
+        {featuresData.map((feature) => (
           <li className="border dark:border-gray-600 border-gray-200 border-solid p-3">
             ✨ {feature}
           </li>
