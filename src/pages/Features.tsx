b529cd49364f38cc66e771c27589bc57c04b3eab
data-miner00 <data-miner00@users.noreply.github.com>
2022-12-05 22:37:43 +0800

    feat: Add ja intl for features

--- src/pages/Features.tsx
+++ src/pages/Features.tsx
@@ -1,18 +1,22 @@
 import React from "react";
+import { useTranslation } from "react-i18next";
+
 import { getVariable, Variable } from "../utils/getVariable";
 import featuresData from "../data/Features.json";
 
 function Features(): JSX.Element {
+  const { t } = useTranslation();
+
   return (
     <div className="max-w-[1400px] mx-auto md:px-8">
       <h1 className="text-xl font-bold mb-10">
-        Detailed list of features that are baked into this template
+        {t("Detailed list of features that are baked into this template")}
       </h1>
 
       <ul className="flex flex-col gap-2">
         {featuresData.map((feature) => (
           <li className="border dark:border-gray-600 border-gray-200 border-solid p-3">
-            ✨ {feature}
+            ✨ {t(feature)}
           </li>
         ))}
       </ul>
