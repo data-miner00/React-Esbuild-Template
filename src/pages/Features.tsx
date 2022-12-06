8597eb6e80907647c943d8f49554f3043b2c3333
data-miner00 <data-miner00@users.noreply.github.com>
2022-12-06 21:10:56 +0800

    style: Fix lint

--- src/pages/Features.tsx
+++ src/pages/Features.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
 
-import { getVariable, Variable } from "../utils/getVariable";
 import featuresData from "../data/Features.json";
 
 function Features(): JSX.Element {
@@ -14,8 +13,11 @@ function Features(): JSX.Element {
       </h1>
 
       <ul className="flex flex-col gap-2">
-        {featuresData.map((feature) => (
-          <li className="border dark:border-gray-600 border-gray-200 border-solid p-3">
+        {featuresData.map((feature, index) => (
+          <li
+            key={index}
+            className="border dark:border-gray-600 border-gray-200 border-solid p-3"
+          >
             ✨ {t(feature)}
           </li>
         ))}
