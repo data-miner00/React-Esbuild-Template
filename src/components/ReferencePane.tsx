bfffd28f833979d8edb28727aca97f1f7586f6e8
data-miner00 <data-miner00@users.noreply.github.com>
2023-01-05 23:34:02 +0800

    feat: Add basic fluid motion to pane

--- src/components/ReferencePane.tsx
+++ src/components/ReferencePane.tsx
@@ -1,43 +1,75 @@
 import { useState } from "react";
 import { useTranslation } from "react-i18next";
+import { motion } from "framer-motion";
 
 import StackCard from "./StackCard";
 import techData from "../data/TechnologyData.json";
+import { referencePaneMotion } from "../motions/referencePaneMotion";
 
 function ReferencePane(): JSX.Element {
   const { t } = useTranslation();
   const [open, setOpen] = useState(true);
+  const [show, setShow] = useState(true);
+  const [loading, setLoading] = useState(false);
 
   function togglePane() {
-    setOpen((previous) => !previous);
+    if (loading) return;
+    setLoading(true);
+    if (open) {
+      setShow((previous) => !previous);
+      setTimeout(() => {
+        setOpen((previous) => !previous);
+        setLoading(false);
+      }, 1500);
+    } else {
+      setOpen((previous) => !previous);
+      setTimeout(() => {
+        setShow((previous) => !previous);
+        setLoading(false);
+      }, 500);
+    }
   }
 
-  return open ? (
-    <aside
-      className="w-96 fixed top-0 left-0 p-3 bg-gray-700 h-screen overflow-y-auto"
-      onClick={togglePane}
-    >
-      <div className="relative h-full">
-        <header className=" bg-gray-700 text-center text-lg font-semibold mb-6 w-full h-7 ">
-          Development Reference
-        </header>
-        <div className="flex flex-col gap-4 items-center">
-          {techData.map((data, index) => (
-            <StackCard
-              key={index}
-              title={data.title}
-              description={t(data.title)}
-              url={data.url}
-            />
-          ))}
-        </div>
-      </div>
-    </aside>
-  ) : (
-    <button
-      className="h-5 w-5 rounded-full bg-gradient-to-br from-[#e0c3fc] to-[#8ec5fc] fixed left-12 bottom-12"
-      onClick={togglePane}
-    ></button>
+  return (
+    <>
+      {open && (
+        <motion.aside
+          initial={false}
+          animate={show ? "open" : "closed"}
+          className={`w-96 fixed top-0 left-0 bg-gray-700 h-screen overflow-y-auto`}
+          onClick={togglePane}
+        >
+          <motion.div
+            variants={referencePaneMotion}
+            className="relative h-full"
+          >
+            <header className="text-center text-lg font-semibold w-full py-8">
+              Development Reference
+            </header>
+            <div>
+              <div
+                className="flex flex-col gap-4 items-center overflow-y-scroll"
+                style={{ height: "calc(100vh - 92px)" }}
+              >
+                {techData.map((data, index) => (
+                  <StackCard
+                    key={index}
+                    title={data.title}
+                    description={t(data.title)}
+                    url={data.url}
+                  />
+                ))}
+              </div>
+            </div>
+          </motion.div>
+        </motion.aside>
+      )}
+
+      <button
+        className="h-7 w-7 rounded-full bg-gradient-to-br from-[#e0c3fc] to-[#8ec5fc] fixed left-12 top-12"
+        onClick={togglePane}
+      ></button>
+    </>
   );
 }
 
