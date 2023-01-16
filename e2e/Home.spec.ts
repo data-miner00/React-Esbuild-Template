b24ef774912ce84d38c2839b00fd5bf2b3daacfb
data-miner00 <data-miner00@users.noreply.github.com>
2023-01-16 23:29:19 +0800

    test: Added simple homepage e2e tests

---
+++ e2e/Home.spec.ts
@@ -0,0 +1,21 @@
+import { test, expect } from "@playwright/test";
+
+const baseUrl = "http://localhost:3000/";
+
+test.describe("Home page", () => {
+  test("has title", async ({ page }) => {
+    await page.goto(baseUrl);
+
+    await expect(page).toHaveTitle(/React ESBuild/);
+  });
+
+  test("open reference pane when click on pane switch", async ({ page }) => {
+    await page.goto(baseUrl);
+
+    await page.getByTestId("pane-switch").click();
+
+    const pane = page.getByTestId("pane");
+
+    await expect(pane).toContainText(/Development Reference/);
+  });
+});
