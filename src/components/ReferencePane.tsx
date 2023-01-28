260114b152d3375c642ad2b0c7c0de985faf51d7
data-miner00 <data-miner00@users.noreply.github.com>
2023-01-28 22:57:35 +0800

    feat: Added pane overlay

--- src/components/ReferencePane.tsx
+++ src/components/ReferencePane.tsx
@@ -6,40 +6,46 @@ import techData from "../data/TechnologyData.json";
 
 function ReferencePane(): JSX.Element {
   const { t } = useTranslation();
-  const [open, setOpen] = useState(!false);
+  const [open, setOpen] = useState(false);
 
   return (
     <>
       {open && (
-        <aside
-          data-testid="pane"
-          className="w-96 fixed top-0 left-0 bg-gray-100 dark:bg-gray-700 h-screen overflow-y-auto border-gray-200 dark:border-gray-600 border-r border-solid"
+        <div
+          className="fixed inset-0 bg-gray-50/80 dark:bg-gray-700/80 z-10"
+          onClick={() => setOpen((open) => !open)}
         >
-          <header className="border-b border-solid border-gray-200 dark:border-gray-600 flex">
-            <div className="border-r border-solid border-gray-200 dark:border-gray-600 w-16"></div>
-            <div className="p-5">Reference Panel</div>
-          </header>
-          <main>
-            <section className="p-5 border-b border-solid border-gray-200 dark:border-gray-600 bg-gray-300/50 dark:bg-gray-800/50">
-              <p>
-                This component serves as a quicklinks to the devtools used
-                within the template. Feel free to delete this component if you
-                don't need it or after your development.
-              </p>
-            </section>
-            <nav className="flex my-5">
-              <button className="block px-5 border-r border-solid border-gray-600">
-                Docs Links
-              </button>
-              <button className="block px-5">Template Features</button>
-            </nav>
-          </main>
-        </aside>
+          <aside
+            onClick={(event) => event.stopPropagation()}
+            data-testid="pane"
+            className="w-96 bg-gray-100 dark:bg-gray-700 h-screen overflow-y-auto border-gray-200 dark:border-gray-600 border-r border-solid"
+          >
+            <header className="border-b border-solid border-gray-200 dark:border-gray-600 flex">
+              <div className="border-r border-solid border-gray-200 dark:border-gray-600 w-16"></div>
+              <div className="p-5">Reference Panel</div>
+            </header>
+            <main>
+              <section className="p-5 border-b border-solid border-gray-200 dark:border-gray-600 bg-gray-300/50 dark:bg-gray-800/50">
+                <p>
+                  This component serves as a quicklinks to the devtools used
+                  within the template. Feel free to delete this component if you
+                  don't need it or after your development.
+                </p>
+              </section>
+              <nav className="flex my-5">
+                <button className="block px-5 border-r border-solid border-gray-600">
+                  Docs Links
+                </button>
+                <button className="block px-5">Template Features</button>
+              </nav>
+            </main>
+          </aside>
+        </div>
       )}
 
       <button
         data-testid="pane-switch"
-        className="h-10 w-10 rounded-full flex place-items-center fixed left-5 top-3"
+        className="h-10 w-10 rounded-full flex place-items-center fixed left-5 top-3 z-20"
         onClick={() => setOpen((open) => !open)}
       >
         <RxHamburgerMenu className="block" size="24px" />
