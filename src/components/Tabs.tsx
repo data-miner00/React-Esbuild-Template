2e84b89b126394f3355b9fae651963d8af07eb1c
data-miner00 <data-miner00@users.noreply.github.com>
2023-01-29 17:57:52 +0800

    feat: Added tab component

---
+++ src/components/Tabs.tsx
@@ -0,0 +1,57 @@
+import { Component } from "react";
+
+type TabProps = {
+  title: string;
+  content: React.ReactNode;
+};
+
+type Props = {
+  tabs: TabProps[];
+};
+
+type States = {
+  activeTab: number;
+};
+
+class Tabs extends Component<Props, States> {
+  constructor(props: Props) {
+    super(props);
+
+    this.state = {
+      activeTab: 0,
+    };
+  }
+
+  onClickTabItem(tabIndex: number) {
+    this.setState({ activeTab: tabIndex });
+  }
+
+  render() {
+    const {
+      props: { tabs },
+      state: { activeTab },
+    } = this;
+
+    return (
+      <div className="tabs">
+        <nav className="flex py-5 border-b border-solid dark:border-gray-600 border-gray-200">
+          {tabs.map((tab, index) => {
+            const activeStyle = index === activeTab ? "text-pink-400" : "";
+            return (
+              <button
+                key={tab.title}
+                onClick={() => this.setState({ activeTab: index })}
+                className={`${activeStyle} block px-5`}
+              >
+                {tab.title}
+              </button>
+            );
+          })}
+        </nav>
+        <div className="py-5">{tabs[activeTab].content}</div>
+      </div>
+    );
+  }
+}
+
+export default Tabs;
