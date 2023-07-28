acd75e7bfb7717e8171335e3f57380bbec611f1f
data-miner00 <data-miner00@users.noreply.github.com>
2023-07-28 21:34:56 +0800

    style: Fix linting

--- src/reportWebVitals.ts
+++ src/reportWebVitals.ts
@@ -1,14 +1,16 @@
 import { ReportCallback } from "web-vitals";
 
-const reportWebVitals = (onPerfEntry?: ReportCallback) => {
-  if (onPerfEntry) {
-    import("web-vitals").then(({ onCLS, onFCP, onFID, onLCP, onTTFB }) => {
-      onCLS(onPerfEntry);
-      onFID(onPerfEntry);
-      onFCP(onPerfEntry);
-      onLCP(onPerfEntry);
-      onTTFB(onPerfEntry);
-    });
+const reportWebVitals = (onPerfEntry?: ReportCallback): void => {
+  if (onPerfEntry != null) {
+    import("web-vitals")
+      .then(({ onCLS, onFCP, onFID, onLCP, onTTFB }) => {
+        onCLS(onPerfEntry);
+        onFID(onPerfEntry);
+        onFCP(onPerfEntry);
+        onLCP(onPerfEntry);
+        onTTFB(onPerfEntry);
+      })
+      .catch(console.error);
   }
 };
 
